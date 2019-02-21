f6d06962b569e6759156ef3d54abac1e5961972c
harkiramadhan <harkiramadhan@gmail.com>
2019-02-21 18:18:12 +0700

    commit

---
+++ application/controllers/Phpmailer.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Phpmailer extends CI_Controller{
+    
+    function  __construct(){
+        parent::__construct();
+    }
+    function index(){
+        $this->load->view('email');
+    }
+    function send(){
+        $this->load->library('PHPMailer_lib');
+        $mail = $this->phpmailer_lib->load();
+        
+        $mail->isSMTP();
+        $mail->Host     = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'harkiramadhan@gmail.com';
+        $mail->Password = 'jakartaselatan';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port     = 465;
+        
+        $mail->setFrom('harkiramadhan@gmail.com', 'HarkiRamadhan');
+        $mail->addReplyTo('info@harkiramadhan.com', 'HarkiRamadhan');
+        
+        $mail->addAddress('madanzdanz@gmail.com');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+        
+        $mail->Subject = 'Send Email via SMTP using PHPMailer in CodeIgniter';
+        
+        $mail->isHTML(true);
+        $mailContent = "<h1>Send HTML Email using SMTP in CodeIgniter</h1>
+            <p>This is a test email sending using SMTP mail server with PHPMailer.</p>";
+        $mail->Body = $mailContent;
+
+        if(!$mail->send()){
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        }else{
+            echo 'Message has been sent';
+        }
+    }
+    
+}
