70176ada15cab744c05c7407684c8d45bbc8c9a6
harkiramadhan <harkiramadhan@gmail.com>
2019-02-23 01:43:24 +0700

    again

--- application/controllers/ItemController.php
+++ application/controllers/ItemController.php
@@ -36,7 +36,8 @@ class ItemController extends CI_Controller {
    */
    public function ajaxSave()
    {  
-      $this->load->library('PHPMailer_lib'); 
+      $this->load->library('PHPMailer_lib');
+      $mail = $this->phpmailer_lib->load();
 
       $tanggal_terima      = $this->input->post('tanggal_terima');   
       $agenda              = $this->input->post('agenda');
@@ -50,30 +51,33 @@ class ItemController extends CI_Controller {
       $tanggal_keluar      = $this->input->post('tanggal_keluar');
       $penerima            = $this->input->post('penerima');
       $gh                  = $this->input->post('gh');
-      $email               = implode(',', $gh);
       $disposisi           = $this->input->post('disposisi');
       $document            = $this->input->post('document');
-      $new                 = explode(",", $email);
-
-      $mail                = $this->phpmailer_lib->load();
-      $mail->isSMTP();
-      $mail->Host          = 'smtp.gmail.com';
-      $mail->SMTPAuth      = true;
-      $mail->Username      = 'harkiramadhan@gmail.com';
-      $mail->Password      = 'jakartaselatan';
-      $mail->SMTPSecure    = 'ssl';
-      $mail->Port          = 465;
-
-      $mail->setFrom('harkiramadhan@gmail.com', 'HarkiRamadhan');
-      $mail->addReplyTo('madanzdanz@gmail.com', 'Madanzdanz');
-      $mail->Subject       = 'Persuratan Masuk Group';
-
-      foreach ($new as $addr) {
+      $email               = explode(",", $gh);
+      
+      foreach ($email as $addr) {
          $mail->addAddress($addr);
       }
 
-      $mail->isHtml(true);
-      $mailcontent         = "
+      $mail->isSMTP();
+      $mail->Host       = 'smtp.gmail.com';
+      $mail->SMTPAuth   = true;
+      $mail->Username   = 'harkiramadhan@gmail.com';
+      $mail->Password   = 'jakartaselatan';
+      $mail->SMTPSecure = 'ssl';
+      $mail->Port       = 465;
+        
+      $mail->setFrom('harkiramadhan@gmail.com', 'HarkiRamadhan');
+      $mail->addReplyTo('info@harkiramadhan.com', 'HarkiRamadhan');
+        
+      $mail->addAddress('madanzdanz@gmail.com');
+      $mail->addCC('cc@example.com');
+      $mail->addBCC('bcc@example.com');
+        
+      $mail->Subject = 'Send Email via SMTP using PHPMailer in CodeIgniter';
+        
+      $mail->isHTML(true);
+      $mailContent = "
          <h2>Persuratan Masuk Group</h2>
          <table>
             <tr><td>Dari</td>                  <td>:</td> <td><b>$dari</b></td></tr>
@@ -83,27 +87,33 @@ class ItemController extends CI_Controller {
             <tr><td>Download Document</td>     <td>:</td> <td><b>$document</b></td> </tr>
          </table>
          <h4>Do Not Reply To This Email</h4>
-      ";     
-      $mail->msgHTML($mailcontent);
-      $mail->send();
+      ";
+      $mail->Body = $mailContent;
 
-         $data = array(
-            "tanggal_terima"        => $this->input->post('tanggal_terima'),
-            "agenda"                => $this->input->post('agenda'),
-            "nomor_surat"           => $this->input->post('nomor_surat'),
-            "tanggal_surat"         => $this->input->post('tanggal_surat'),
-            "dari"                  => $this->input->post('dari'),
-            "kepada"                => $this->input->post('kepada'),
-            "perihal"               => $this->input->post('perihal'),
-            "dodate"                => $this->input->post('dodate'),
-            "jenis"                 => $this->input->post('jenis'),
-            "tanggal_keluar"        => $this->input->post('tanggal_keluar'),
-            "penerima"              => $this->input->post('penerima'),
-            "gh"                    => $this->input->post('gh'),
-            "disposisi"             => $this->input->post('disposisi'),
-            "document"              => $this->input->post('document'),
-        );
-        $this->db->insert('surat_masuk', $data);
+      if(!$mail->send()){
+         echo 'Message could not be sent.';
+         echo 'Mailer Error: ' . $mail->ErrorInfo;
+      }else{
+            echo 'Message has been sent';
+      }
+
+      //    $data = array(
+      //       "tanggal_terima"        => $this->input->post('tanggal_terima'),
+      //       "agenda"                => $this->input->post('agenda'),
+      //       "nomor_surat"           => $this->input->post('nomor_surat'),
+      //       "tanggal_surat"         => $this->input->post('tanggal_surat'),
+      //       "dari"                  => $this->input->post('dari'),
+      //       "kepada"                => $this->input->post('kepada'),
+      //       "perihal"               => $this->input->post('perihal'),
+      //       "dodate"                => $this->input->post('dodate'),
+      //       "jenis"                 => $this->input->post('jenis'),
+      //       "tanggal_keluar"        => $this->input->post('tanggal_keluar'),
+      //       "penerima"              => $this->input->post('penerima'),
+      //       "gh"                    => $this->input->post('gh'),
+      //       "disposisi"             => $this->input->post('disposisi'),
+      //       "document"              => $this->input->post('document'),
+      //   );
+      //   $this->db->insert('surat_masuk', $data);
    }
    public function ajaxRequestPost(){
       $data = array(
